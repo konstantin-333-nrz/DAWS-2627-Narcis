15c084ee238f6d21976c29e9a55c24a20d58eb10
Your Name <you@example.com>
2026-09-16 20:17:49 +0200

    16/09

--- Tema1/ejercicios/ejercicio1/info_basica.php
+++ Tema1/ejercicios/ejercicio1/info_basica.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio 1</title>
+    <link rel="stylesheet" href="info_basica.css">
 </head>
 <body>
     <?php
         $name = "Narcis Constantin Dragomir";
         $year= 2006;
     ?>
-    <div>
-        <p style="color:green">
+    <div id="contenedor">
+        <p id= "parrafo">
             Hola me llamo <?php print $name;?> y nací el año <?php echo $year?>
         </p>
     </div>
