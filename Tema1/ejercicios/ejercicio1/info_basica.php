aedd71958d908d6d84d9ad10d1c069c278eb0455
Your Name <you@example.com>
2026-09-16 20:06:14 +0200

    16/09 ejercicio baasico

---
+++ Tema1/ejercicios/ejercicio1/info_basica.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <?php
+        $name = "Narcis Constantin Dragomir";
+        $year= 2006;
+    ?>
+    <div>
+        <p>Hola me llamo <?php print $name;?> y nací el año <?php echo $year?></p>
+    </div>
+</body>
+</html>
