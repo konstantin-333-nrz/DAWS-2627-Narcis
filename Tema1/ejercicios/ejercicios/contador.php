a4a1a52b16fc8a59a79762d7d7420b420ced720c
Your Name <you@example.com>
2026-09-22 18:42:14 +0200

    contador

---
+++ Tema1/ejercicios/ejercicios/contador.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contador</title>
+</head>
+<body>
+    <?php
+        
+        for($cont = 1; $cont <= 100; $cont++){
+            echo $cont;
+        }
+    ?>
+</body>
+</html>
