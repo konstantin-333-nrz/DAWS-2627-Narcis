4c5321721ce772757ff3ce3794ed0f6df9f10c33
Your Name <you@example.com>
2026-09-22 17:42:30 +0200

    añadiendo CSS

--- Tema1/ejercicios/ejercicios/prueba_if.php
+++ Tema1/ejercicios/ejercicios/prueba_if.php
@@ -4,6 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prueba if</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 60px 30px;
+            background-color: #f4f4f4;
+            color: #202124;
+            font-family: Georgia, serif;
+        }
+        
+         h1 {
+            max-width: 700px;
+            margin: 0 auto 35px;
+            padding: 25px 30px;
+            background-color: #202124;
+            border-bottom: 6px solid #5f6368;
+            color: #ffffff;
+            font-family: Arial, sans-serif;
+            font-size: 30px;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <?php
