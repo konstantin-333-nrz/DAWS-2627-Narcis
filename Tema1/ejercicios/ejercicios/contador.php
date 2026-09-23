3d09f5c387215b5dd2d6797a7676341248159d80
NarcisDragomir <narciscarp3@gmail.com>
2026-09-23 11:20:45 +0200

    añadir css

--- Tema1/ejercicios/ejercicios/contador.php
+++ Tema1/ejercicios/ejercicios/contador.php
@@ -4,6 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contador</title>
+     <style>
+        body {
+            margin: 40px;
+            font-family: Arial, sans-serif;
+            color: #333;
+            background-color: #f4f4f4;
+            line-height: 1.8;
+        }
+
+        hr {
+            margin: 25px 0;
+            border: 0;
+            border-top: 2px solid #3498db;
+        }
+    </style>
 </head>
 <body>
     <?php
