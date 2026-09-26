25f2cadda25c5da193b4552c56496770526042fb
NarcisDragomir <narciscarp3@gmail.com>
2026-09-26 18:36:37 +0200

    prueba s

--- Tema1/ejercicios/ejer_array/coches.php
+++ Tema1/ejercicios/ejer_array/coches.php
@@ -6,6 +6,22 @@
     <title>Mi garaje</title>
 </head>
 <body>
-    
+    <h1>Mi garaje</h1>
+    <?php
+        $matriculas = array(
+            "7091BRZ" => array("Opel", "Astra", 3),
+            "1011LSF" => array("Opel", "Astra", 5),
+            "1235ANS" => array("Citroen", "C4", 5),
+            "1111BDC" => array("Renault", "Clio", 3),
+            "2909GJN" => array("Citroen", "Berlingo")
+        );
+            
+        sort($matriculas);
+
+        foreach($matriculas as $matricula){
+            echo "<h1>" . $matricula . "</h1>";
+        }
+
+    ?>
 </body>
 </html>
