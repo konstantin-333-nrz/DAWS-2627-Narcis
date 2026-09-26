a3ff0b150adf758c7c80908d043b69f47fd20894
NarcisDragomir <narciscarp3@gmail.com>
2026-09-26 19:02:17 +0200

    Implementacion del CSS

--- Tema1/ejercicios/ejer_array/coches.php
+++ Tema1/ejercicios/ejer_array/coches.php
@@ -4,6 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi garaje</title>
+    <style> 
+        body {
+                margin: 0;
+                padding: 30px;
+                background-color: #f2f2f2;
+                color: #333;
+                font-family: Arial, sans-serif;
+            }
+
+            h1 {
+                text-align: center;
+                color: #444;
+                font-weight: normal;
+            }
+
+            table {
+                width: 80%;
+                margin: 20px auto;
+                background-color: white;
+                border-collapse: collapse;
+            }
+
+            th,
+            td {
+                padding: 12px;
+                text-align: left;
+                border: 1px solid #ccc;
+            }
+
+            th {
+                background-color: #ddd;
+                color: #444;
+            }
+
+            tr:nth-child(even) {
+                background-color: #f8f8f8;
+            }
+    </style>
 </head>
 <body>
     <h1>Mi garaje</h1>
